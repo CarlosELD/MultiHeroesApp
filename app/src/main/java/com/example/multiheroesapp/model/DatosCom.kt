7266fda832da431826b7c7eaf4233f89602ea953
CarlosELD <89476876+CarlosELD@users.noreply.github.com>
2023-09-23 15:20:51 -0600

    SE agrego la clase DatosCom que incluye un metodo llamado AlmacenGod con una variable llamada Canon

---
+++ app/src/main/java/com/example/multiheroesapp/model/DatosCom.kt
@@ -0,0 +1,37 @@
+package com.example.multiheroesapp.model
+
+import com.example.multiheroesapp.R
+object AlmacenGod{
+    val Canon = listOf(
+        Hero(
+            nombreCal = R.string.hero1,
+            descripcionHe = R.string.description1,
+            picturesHe = R.drawable.android_superhero1
+        ),
+        Hero(
+            nombreCal = R.string.hero2,
+            descripcionHe = R.string.description2,
+            picturesHe = R.drawable.android_superhero2
+        ),
+        Hero(
+            nombreCal = R.string.hero3,
+            descripcionHe = R.string.description3,
+            picturesHe = R.drawable.android_superhero3
+        ),
+        Hero(
+            nombreCal = R.string.hero4,
+            descripcionHe = R.string.description4,
+            picturesHe = R.drawable.android_superhero4
+        ),
+        Hero(
+            nombreCal = R.string.hero5,
+            descripcionHe = R.string.description5,
+            picturesHe = R.drawable.android_superhero5
+        ),
+        Hero(
+            nombreCal = R.string.hero6,
+            descripcionHe = R.string.description6,
+            picturesHe = R.drawable.android_superhero6
+        )
+    )
+}
