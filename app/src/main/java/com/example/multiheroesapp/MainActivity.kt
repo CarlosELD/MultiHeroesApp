1f380f96780b7636a7cec2d574ff79a528b60ee9
CarlosELD <89476876+CarlosELD@users.noreply.github.com>
2023-09-23 17:50:39 -0600

    Se agregaron dos métodos en el mainActivity y se creo una clase llamada HerosScreen que incluye dos métodos uno llamado ObjetoClave y otro llamado ListaCompac

--- app/src/main/java/com/example/multiheroesapp/MainActivity.kt
+++ app/src/main/java/com/example/multiheroesapp/MainActivity.kt
@@ -1,14 +1,19 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.multiheroesapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.multiheroesapp.ui.theme.MultiHeroesAppTheme
 class MainActivity : ComponentActivity() {
@@ -16,7 +21,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MultiHeroesAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -36,10 +40,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CimaTotal(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge,
+            )
+        },
+        modifier = modifier
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SuperPreview() {
     MultiHeroesAppTheme {
-        Greeting("Android")
+        Greeting(name = "")
     }
 }
